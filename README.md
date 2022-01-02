15bb20ef46ee216d46e9a21602564498f4f00db9
morizkay <morizkay@live.com>
2022-01-02 23:28:19 +0800

    RMRF

--- README.md
+++
@@ -1,1 +0,0 @@
-# hepi-frontend
--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Router from "./app/pages/Router";
-
-const App: React.FC = (): JSX.Element => {
-  return <Router />;
-};
-
-export default App;
