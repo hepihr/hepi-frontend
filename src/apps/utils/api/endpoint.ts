682ae08865a5d1b473ae4c0e53b27bd562fac400
morizkay <morizkay@live.com>
2022-01-06 11:15:37 +0800

    Set api and endpoint

---
+++ src/apps/utils/api/endpoint.ts
@@ -0,0 +1,8 @@
+export const timeoffEmployee = "timeoff/employees/{employeeId}";
+export const timeoffEmployees = "timeoff/employees";
+
+export const timeoffHitorys = "timeoff/hitorys";
+export const timeoffHitory = "timeoff/hitorys/{hitoryId}";
+
+export const timeoffRequests = "timeoff/requests";
+export const timeoffRequest = "timeoff/requests/{requestId}";
