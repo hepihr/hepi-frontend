899d7f6a7b71855cb5734ae63af27d7f47ca633f
morizkay <morizkay@live.com>
2022-01-07 09:29:31 +0800

    XXX

--- src/apps/pages/LandingPage.tsx
+++ src/apps/pages/LandingPage.tsx
@@ -1,33 +1,11 @@
-import { Breadcrumb, Layout, Menu } from "antd";
 import React from "react";
 
-const LandingPage: React.FC = (): JSX.Element => {
+const LandingPage = () => {
   return (
-    <Layout className="layout">
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
-          {new Array(15).fill(null).map((_, index) => {
-            const key = index + 1;
-            return <Menu.Item key={key}>{`nav ${key}`}</Menu.Item>;
-          })}
-        </Menu>
-      </Header>
-      <Content style={{ padding: "0 50px" }}>
-        <Breadcrumb style={{ margin: "16px 0" }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content">Content</div>
-      </Content>
-      <Footer style={{ textAlign: "center" }}>
-        Ant Design ©2018 Created by Ant UED
-      </Footer>
-    </Layout>
+    <div>
+      <h1>Landing Page</h1>
+    </div>
   );
 };
 
 export default LandingPage;
-
-const { Header, Content, Footer } = Layout;
--- src/apps/utils/api/endpoint.ts
+++ src/apps/utils/api/endpoint.ts
@@ -1,3 +1,47 @@
 export const timeoffEmployeesAPI = "timeoff/employees";
 export const timeoffHitorysAPI = "timeoff/hitorys";
 export const timeoffRequestsAPI = "timeoff/requests";
+
+
+        "/timeoff/date":{
+            "get":{
+         
+        },
+        "/timeoff/requested/{employee_id}":{
+            "put":{
+              
+        },
+        "/timeoff/requested":{
+            "get":{
+                
+        },
+        "/timeoff":{
+            "post":{
+               
+            "get":{
+            
+        "/employees/{employeeId}/timeoff":{
+            "get":{
+               
+            "post":{
+               
+        "/users": {
+            "get":{
+                
+        "/employees/{employeeId}/users":{
+            "post":{
+               
+            "get":{
+                
+        "/employees":{
+          
+            "post":{
+               
+            "get":{
+               
+        "/positions/{id}":{
+           
+        "/positions":{
+          
+            "get":{
+                
